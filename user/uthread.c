fc2063914940b07186ba810830c7496c920c9d8a
poiyth <3505912394@qq.com>
2025-09-12 23:02:27 +0800

    Uthread finished

--- user/uthread.c
+++ user/uthread.c
@@ -11,9 +11,31 @@
 #define MAX_THREAD  4
 
 
+struct context 
+{
+  //特殊寄存器
+  uint64 ra;
+  //callee-save寄存器
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
+
+
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context ctx;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -62,6 +84,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)&(t->ctx), (uint64)&(current_thread->ctx));   //这里添加代码
   } else
     next_thread = 0;
 }
@@ -76,6 +99,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->ctx.ra = (uint64)func;
+  t->ctx.sp = (uint64)(t->stack + STACK_SIZE - 1);
 }
 
 void 
